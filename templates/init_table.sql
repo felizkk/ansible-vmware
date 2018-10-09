f80dd1ca57fd84bb5e7dbddae21e36e58f8d43d1
root <root@tower.felsys.example.com>
2018-10-09 03:58:05 -0700

    update

--- templates/init_table.sql
+++ templates/init_table.sql
@@ -3,11 +3,12 @@ CREATE TABLE `member` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(30) NOT NULL,
   `surname` varchar(30) NOT NULL,
+  `role` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `member` WRITE;
-INSERT INTO `member` (`firstname`, `surname`) VALUES ('Feliz','Karnadi');
-INSERT INTO `member` (`firstname`, `surname`) VALUES ('Grant','Du Preez');
+INSERT INTO `member` (`firstname`, `surname`, `role`) VALUES ('Feliz','Karnadi', 'Solutions Architect');
+INSERT INTO `member` (`firstname`, `surname`, `role`) VALUES ('Grant','Du Preez', 'Business Development Manager');
 UNLOCK TABLES;
 
